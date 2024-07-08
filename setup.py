8b5a7c763ca500b35f5444d57ea40d4874371c40
Zachary Ivie <zachary.ivie@gmail.com>
2024-07-08 00:40:18 -0400

    updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SwiftLLM',
-    version='0.0.2',
+    version='0.0.3',
     author='Zachary Ivie',
     author_email='zachary.ivie@gmail.com',
     description='A python package to access most common foundation models easily.',
