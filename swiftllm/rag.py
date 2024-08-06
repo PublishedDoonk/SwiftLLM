eb24440fa23f0abc9f94cba9ccd14e68cbdeaef8
PublishedDoonk <zachary.ivie@gmail.com>
2024-08-06 19:16:03 -0400

    updated gitignore and set openai embeddings to default

--- swiftllm/rag.py
+++ swiftllm/rag.py
@@ -1,14 +1,19 @@
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
-from langchain.embeddings import SentenceTransformersEmbeddings
+#from langchain_huggingface import HuggingFaceEmbeddings
+#from sentence_transformers import SentenceTransformer
+#from langchain_community.embeddings import SentenceTransformerEmbeddings
+from dotenv import load_dotenv
 from re import Pattern
+from shutil import rmtree
 import os
 
 
 class RAG:
 
-    def __init__(self, data_path: str = 'data', file_exts: Pattern | str = None, db_path: str = 'chroma', embeddings: str = 'SentenceTransformersEmbeddings'):
+    def __init__(self, data_path: str = 'data', file_exts: Pattern | str = None, db_path: str = 'chroma', embeddings: str = 'OpenAIEmbeddings'):
+        load_dotenv()
         self.data_path = self.set_data_path(data_path)
         self.file_exts = self.set_file_exts(file_exts)
         self.db_path = self.set_db_path(db_path)
@@ -17,34 +22,42 @@ class RAG:
     def set_data_path(self, data_path: str):
         if not data_path:
             data_path = 'data'
-        if os.path.isdir(data_path):
-            return data_path
         if os.path.isfile(data_path):
             self.file_exts = os.path.basename(data_path)
             return os.path.dirname(data_path)
+        if not os.path.isdir(data_path):
+            os.mkdir(data_path)
+        if os.path.isdir(data_path):
+            return data_path
         raise ValueError(f'{data_path} is not a valid path or does not have read access.')
 
-    def set_file_exts(self, file_exts: Pattern | str):
+    def set_file_exts(self, file_exts: Pattern | str) -> str:
         if file_exts is None:
-            return r'.*\.md'
-        if isinstance(file_exts, str):
-            return r'.*' + file_exts
-        if not isinstance(file_exts, Pattern):
+            return "*"
+        if isinstance(file_exts, Pattern):
+            return f"{file_exts}"
+        if not isinstance(file_exts, str):
             raise ValueError(f'file_exts must be a string or a compiled regular expression.')
         return file_exts
     
     def set_db_path(self, db_path: str):
         if os.path.isdir(db_path):
-            os.removedirs(db_path)
+            rmtree(db_path)
         os.mkdir(db_path)
 
         return db_path
 
     def set_embeddings(self, embedding: str):
-        if embedding == 'SentenceTransformersEmbeddings':
-            return SentenceTransformersEmbeddings(model_name='all-MiniLM-L6-v2')
+        if embedding == 'SentenceTransformerEmbeddings':
+            pass # TODO: Troubleshoot SentenceTransformerEmbeddings import errors
+            #return SentenceTransformer()
         if embedding == 'OpenAIEmbeddings':
             return OpenAIEmbeddings()
         raise ValueError(f'Embedding {embedding} not supported.')
 
-    
+if __name__ == '__main__':
+    rag = RAG()
+    print(rag.data_path)
+    print(rag.file_exts)
+    print(rag.db_path)
+    print(rag.embeddings)
